aeba0ca16aeec7a57c301ebc8cb092a6ccf30594
LowHongWei <s10203927@connect.np.edu.sg>
2022-09-01 23:44:23 +0800

    Centered logo for the nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-
+import Image from 'next/image'
 const Home = () => {
   return ( 
     <div>
-      <h1>
-        Hello
-      </h1>
+      <h1>Hello</h1>
     </div>
   );
 }
+<img src='/flush.png' />
  
 export default Home;
